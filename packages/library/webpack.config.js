36962f74559f40ddf50e90c8f264a0ae4d56c4e6
Shubham Shah <shubhamshahrising@gmail.com>
2023-12-01 11:53:19 +0530

    Fix issue with monorepo

--- packages/library/webpack.config.js
+++ packages/library/webpack.config.js
@@ -15,6 +15,11 @@ const config = {
     library: "library",
     libraryTarget: "umd",
   },
+  // exclude react and react-dom from output
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
   devServer: {
     open: true,
     host: "localhost",
